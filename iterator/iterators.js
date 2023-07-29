025c724c36937782b067df5c37915300ba13b180
habib <habibmhr143@gmail.com>
2023-07-29 12:10:49 +0600

    use strict

---
+++ iterator/iterators.js
@@ -0,0 +1,12 @@
+// let array = [1, 2, 3, 4, 5, 6];
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+let array = [1, 2, 3, 4, 5, 6];
+console.dir(array);
+
+// for (newArray of array) {
+//   console.log(newArray);
+// }
---
+++ use_strict.js
@@ -0,0 +1,2 @@
+"use strict";
+let name = "Hi";
