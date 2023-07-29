1c4a08f03a2b81f5707f68cef98ab0ab17555f06
habib <habibmhr143@gmail.com>
2023-07-29 11:41:22 +0600

    arrow functon

---
+++ arrrow_function.js
@@ -0,0 +1,13 @@
+// function expression
+// let fun = function (x, y) {
+//   return x + y;
+// };
+// console.log(fun("a", "b"));
+
+// arrow function
+
+let fun = (x, y) => {
+  return x + y;
+};
+
+console.log(fun(10, 20));
