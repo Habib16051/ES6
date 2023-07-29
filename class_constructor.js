ce3b96d9f24243ef3b36284711ad155089eae69b
habib <habibmhr143@gmail.com>
2023-07-29 10:43:02 +0600

    class constructor

---
+++ class_constructor.js
@@ -0,0 +1,9 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+let BookDetails = new Book("Django 4 by Example", "Gilbert Burns");
+
+console.log(BookDetails);
